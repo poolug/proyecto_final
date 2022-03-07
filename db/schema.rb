b40cbd5675d3fdc1b58ce5a4dce8f28b66ccaff2
poolug <pauluzcategui43@gmail.com>
2022-03-07 20:19:15 -0300

    Uso de gemset default y cambio a postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2022_03_06_194221) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "housing_users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "housing_id"
-    t.integer "user_id"
+    t.bigint "housing_id"
+    t.bigint "user_id"
     t.index ["housing_id"], name: "index_housing_users_on_housing_id"
     t.index ["user_id"], name: "index_housing_users_on_user_id"
   end
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2022_03_06_194221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_housings_on_user_id"
   end
 
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 2022_03_06_194221) do
     t.integer "type_transaction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -65,4 +68,8 @@ ActiveRecord::Schema.define(version: 2022_03_06_194221) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "housing_users", "housings"
+  add_foreign_key "housing_users", "users"
+  add_foreign_key "housings", "users"
+  add_foreign_key "transactions", "users"
 end
