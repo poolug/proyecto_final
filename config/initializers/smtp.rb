3c9d8e07620c3435631570e24e2f5afdb3bda801
poolug <pauluzcategui43@gmail.com>
2022-03-23 21:05:42 -0300

    fix env credentials on heroku

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
-  :user_name => '<%= Figaro.env.sendgrid_user_id %>', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => '<%= Figaro.env.sendgrid_api_key %>',
+  :user_name => ENV['sendgrid_user_id'], # This is the string literal 'apikey', NOT the ID of your API key
+  :password => ENV['sendgrid_api_key'],
   :domain => 'https://chanchito-sano-poolug.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
