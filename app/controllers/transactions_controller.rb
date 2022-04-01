275886485587399a180cec7f58c6393828dd8332
poolug <pauluzcategui43@gmail.com>
2022-04-01 08:26:18 -0300

    js para transaction#destroy

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -88,11 +88,8 @@ class TransactionsController < ApplicationController
   # DELETE /transactions/1 or /transactions/1.json
   def destroy
     @transaction.destroy
-
-    respond_to do |format|
-      format.html { redirect_to transactions_url, notice: "Movimiento eliminado." }
-      format.json { head :no_content }
-    end
+    @notice = "Movimiento eliminado."
+    render "transaction_destroy_success.js.erb"
   end
 
   private
