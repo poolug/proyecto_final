81b6ebdb31e627a5b992b7e5812f535f7d31c424
Paul Uzcategui <paul.uzcategui@clickfono.com>
2023-03-31 23:11:10 -0300

    TEMPORAL: quitar configuración de zona horaria (active_record.default_timezone)

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module ProyectoFinal
 
     config.time_zone = 'America/Santiago'
 
-    config.active_record.default_timezone = :local
+    # config.active_record.default_timezone = :local
 
     Groupdate.time_zone = 'America/Santiago'
     # Settings in config/environments/* take precedence over those specified here.
